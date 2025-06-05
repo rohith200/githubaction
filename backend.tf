7a039861d6cc43a81bd25e5a62d0a33bd64577f6
Rohith Kandikatla <kandikatlarohith5189@gmail.com>
2025-06-05 18:00:03 -0500

    Added resource group and storage account for backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-rg"
-    storage_account_name = "tfstatestore123"
+    storage_account_name = "tfstatestore234"
     container_name       = "tfstate"
     key                  = "vm.tfstate"
   }
